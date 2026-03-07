9fe2383df5ac9a01602fc31d1358317d81270b61
william-li0830 <williamli0830@gmail.com>
2026-03-06 21:27:40 -0800

    Initial commit

---
+++ src/main/java/FlashcardBuilder.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author william.li
+ */
+public class FlashcardBuilder {
+
+    public static void main(String[] args) {
+       
+    }
+}
